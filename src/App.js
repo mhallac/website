51cb30698fdb31929ee62d74fc9658b1fad0da42
mhallac <matthew.hallac@berkeley.edu>
2020-10-23 04:02:56 -0400

    added home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from "./components/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Site is under construction!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
-    </div>
+    <Account>
+      <Switch>
+        <Route path="/" component={Home} />
+      </Switch>
+    </Account>
   );
 }
 
