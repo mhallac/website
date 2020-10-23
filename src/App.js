f602ffade43b5b9f8b2d5d430176313d353fb290
mhallac <matthew.hallac@berkeley.edu>
2020-10-23 04:04:34 -0400

    added home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from "./components/home";
+import { Route, Switch } from 'react-router-dom';
+import { Account } from './components/login/Accounts';
 
 function App() {
   return (
